2dfaa0e7283e236f32c6c1288cc0bc6608b2edab
steeltx <realsteel.gnu@gmail.com>
2021-07-11 23:49:03 -0500

    Props en componente de usuario

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <template>
-  <p>Hey!</p>
+  <h3>Hey!</h3>
   <Greeting />
+  <User :age="age" />
 </template>
 
 <script>
 import Greeting from "./components/Greeting.vue";
+import User from "./components/User";
 
 export default {
   name: "App",
   components: {
     Greeting,
+    User,
   },
+  data(){
+    return{
+      age: 20
+    }
+  }
 };
 </script>
